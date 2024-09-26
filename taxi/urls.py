341767fc57604f5ce9e42114a4aa57e7a284d3fb
Vlad Rymarchuk <vladrymarchuk@gmail.com>
2024-09-27 00:24:59 +0300

    Started developing. Created 'class ManufacturerListView in views.py, added url in taxi/urls.py, and created template manufacturer_list.html with sketchy page

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import index
+from .views import index, ManufacturerListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("manufacturer/", ManufacturerListView.as_view(), name="manufacturer-list"),
 ]
 
 app_name = "taxi"
